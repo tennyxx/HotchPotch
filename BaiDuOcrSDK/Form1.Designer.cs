fc8a7ac840159609de993c76726ce814a3e44ac7
刘昌鑫 <liucx_wk@163.com>
2019-07-15 19:00:48 +0800

    行程单和增值税发票demo

---
+++ BaiDuOcrSDK/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BaiDuOcrSDK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ResultText = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "增值税发票";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ResultText
+            // 
+            this.ResultText.Location = new System.Drawing.Point(49, 107);
+            this.ResultText.Multiline = true;
+            this.ResultText.Name = "ResultText";
+            this.ResultText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ResultText.Size = new System.Drawing.Size(436, 592);
+            this.ResultText.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(186, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "火车票";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 772);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ResultText);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ResultText;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BaiDuOcrSDK/Form1.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiDuOcrSDK
+{
+    public partial class Form1 : Form
+    {
+        public static string GetValue(string key)
+        {
+            return ConfigurationManager.AppSettings[key].ToString().Trim();
+        }
+        // APPID
+        private static string appId = GetValue("BaiduAI.AppID");
+        // 百度云中开通对应服务应用的 API Key 建议开通应用的时候多选服务
+        private static String clientId = GetValue("BaiduAI.APIKey");
+        // 百度云中开通对应服务应用的 Secret Key
+        private static String clientSecret = GetValue("BaiduAI.SecretKey");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 增值税发票识别
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public JObject VatInvoiceOcr(string path)
+        {
+            var client = new Baidu.Aip.Ocr.Ocr(clientId, clientSecret);
+            client.Timeout = 60000;
+            var image = File.ReadAllBytes(path);
+            JObject result = new JObject();
+            try
+            {
+                // 调用增值税发票识别，可能会抛出网络等异常，请使用try/catch捕获
+                result = client.VatInvoice(image);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+        public JObject TrainTicketDemo(string path)
+        {
+            var client = new Baidu.Aip.Ocr.Ocr(clientId, clientSecret);
+            client.Timeout = 60000;
+            var image = File.ReadAllBytes(path);
+            JObject result = new JObject();
+            try
+            {
+                // 调用火车票识别，可能会抛出网络等异常，请使用try/catch捕获
+                result = client.TrainTicket(image);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog path = new OpenFileDialog();
+
+            DialogResult dr = path.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                var result = VatInvoiceOcr(path.FileName);
+                ResultText.Text = result.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog path = new OpenFileDialog();
+
+            DialogResult dr = path.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                var result = TrainTicketDemo(path.FileName);
+                ResultText.Text = result.ToString();
+            }
+        }
+    }
+}
